6d2310b8f39afccc8c9dffe9e4cd0e953a72dec0
Lotfullah <85621380+Lotfullah21@users.noreply.github.com>
2024-09-03 00:24:52 +0530

    Delete form-app-1 directory

--- form-app-1/src/data/data.js
+++
@@ -1,8 +0,0 @@
-const Universities = [
-  { id: 1, name: "Massachuset Institute of Technology" },
-  { id: 2, name: "Stanford University" },
-  { id: 3, name: "Cornell University" },
-  { id: 4, name: "Columbia University" },
-];
-
-export default Universities;
