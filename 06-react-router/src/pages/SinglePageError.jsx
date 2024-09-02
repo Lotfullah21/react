b2bb8b9e7dc0b788fa2ba5a6309a71784e261a0b
Lotfullah <andishmandlotfullah21@gmail.com>
2024-09-02 17:30:35 +0530

    readme updated

--- 06-react-router/src/pages/SinglePageError.jsx
+++
@@ -1,7 +0,0 @@
-import { useRouteError } from "react-router-dom";
-
-const SinglePageError = () => {
-  const error = useRouteError();
-  return <h1>{error.message}</h1>;
-};
-export default SinglePageError;
